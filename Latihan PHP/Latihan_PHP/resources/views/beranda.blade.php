899347826eb4939b33dd4683853e82dcab78c75e
Michel <dmaikel10@gmail.com>
2025-04-29 20:32:58 +0700

    Local Host

---
+++ Latihan PHP/Latihan_PHP/resources/views/beranda.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h1>Ini halaman beranda</h1>
+    <h2>Hallo{{ $name }}</h2>
+    <h2>Your Email is {{ $email }}</h2>
+    <h2>Alamat kamu berada di {{ $alamat }}</h2>
+</div>
--- Latihan PHP/Latihan_PHP/routes/web.php
+++ Latihan PHP/Latihan_PHP/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', function () {
+    return view('beranda',['name'=>'Michel','email'=>'dmaikel10@gmail.com','alamat'=>'jalan kutilang V']);
+});
+Route::get('/berita/', function ($id) {
+    return view('berita',['id'=>$id,'judul'=>$judul]);
+});
